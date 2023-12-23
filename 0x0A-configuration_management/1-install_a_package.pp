92e4fe53edcaf28acb37577f0cbeea6cd5155fac
Kenrique _Ngwa <72971484+kenrique100@users.noreply.github.com>
2023-12-23 11:26:50 -0500

    updated commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Using Puppet, install flask from pip3
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# Am also specifying the version of flask to install using the ensure attribute
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => pip3,
 }
